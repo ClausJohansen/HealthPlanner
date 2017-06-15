964ef90ef57b7b0c0ffe1bd93693ff63551e554b
Claus Johansen <myemail@isnot.public>
2017-06-15 19:55:42 +0200

    Backend application genisis

---
+++ HealthPlanner.Backend/HealthPlanner.Backend.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HealthPlanner.Backend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ HealthPlanner.Backend/HealthPlanner.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HealthPlanner.Backend.WebApiApplication" Language="C#" %>

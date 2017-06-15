3362b0c30f094cdd9d0cf647d5cd2d94d9e951bf
Claus Johansen <myemail@isnot.public>
2017-06-15 20:06:47 +0200

    Database connectivity for backend

---
+++ HealthPlanner.Backend/HealthPlanner.Backend/Utility/DatabaseManager.cs
@@ -0,0 +1,24 @@
+﻿using HealthPlanner.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace HealthPlanner.Backend.Utility
+{
+    public class DatabaseManager
+    {
+        public static AccountDb CreateAccountDb()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MongoDb"].ToString();
+            return new AccountDb(connectionString);
+        }
+
+        public static DayPlanDb CreateDayPlanDb()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MongoDb"].ToString();
+            return new DayPlanDb(connectionString);
+        }
+    }
+}
